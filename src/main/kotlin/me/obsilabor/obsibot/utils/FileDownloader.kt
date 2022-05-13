b4c4d7b0a2d5c5d8ded99ca68ea70a6701af7acf
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-13 22:58:12 +0200

    ich sags dir wenn ktor noch einmal muckt werd ich google result 1 java download utility class von stackoverflow reinpasten es ist mir so egal diese drecks klasse macht zum 3. mal probleme jetzt was soll das
    ich sags dir wenn ktor noch einmal muckt werd ich google result 1 java download utility class von stackoverflow reinpasten es ist mir so egal diese drecks klasse macht zum 3. mal probleme jetzt was soll das

--- src/main/kotlin/me/obsilabor/obsibot/utils/FileDownloader.kt
+++ src/main/kotlin/me/obsilabor/obsibot/utils/FileDownloader.kt
@@ -1,11 +1,9 @@
 package me.obsilabor.obsibot.utils
 
-import io.ktor.client.plugins.*
+import io.ktor.client.features.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.util.*
-import io.ktor.util.cio.*
-import io.ktor.utils.io.*
 import kotlinx.coroutines.*
 import java.io.File
 import kotlin.math.roundToInt
@@ -13,9 +11,8 @@ import me.obsilabor.obsibot.ObsiBot
 
 object FileDownloader {
 
-    @OptIn(InternalAPI::class)
     suspend fun downloadFile(url: String, targetFile: File, current: Int, total: Int) {
-        val downloadContent = ObsiBot.ktorClient.get(url) {
+        val downloadContent = ObsiBot.ktorClient.get<HttpResponse>(url) {
             onDownload { bytesSentTotal, contentLength ->
                 val progress = bytesSentTotal.toDouble() / contentLength.toDouble()
                 val hashtags = (progress * 30).roundToInt()
