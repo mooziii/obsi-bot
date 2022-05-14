48e5bf23ed64c7b0d60691808f1f3b2e50bdec1d
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-14 11:13:43 +0200

    Fix ReadyListener [skip ci]

--- src/main/kotlin/me/obsilabor/obsibot/listeners/ReadyListener.kt
+++ src/main/kotlin/me/obsilabor/obsibot/listeners/ReadyListener.kt
@@ -7,6 +7,7 @@ import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.createRole
 import dev.kord.core.event.gateway.ReadyEvent
 import dev.kord.core.event.guild.GuildCreateEvent
+import kotlinx.coroutines.flow.toList
 import me.obsilabor.obsibot.ObsiBot
 import me.obsilabor.obsibot.utils.createObsiGuild
 import me.obsilabor.obsibot.utils.obsify
@@ -20,16 +21,14 @@ class ReadyListener : Extension() {
         event<ReadyEvent> {
             action {
                 ObsiBot.client = kord
-            }
-        }
-        event<GuildCreateEvent> {
-            action {
-                val obsiGuild = event.guild.obsify() ?: event.guild.createObsiGuild()
-                if(obsiGuild.giveawayRole == null || event.guild.getRoleOrNull(obsiGuild.giveawayRole ?: Snowflake(0)) == null) {
-                    obsiGuild.giveawayRole = event.guild.createRole {
-                        name = "Giveaway Permissions"
-                        reason = "Created by obsi-bot (please don't delete)"
-                    }.id
+                kord.guilds.toList().forEach {
+                    val obsiGuild = it.obsify() ?: it.createObsiGuild()
+                    if(obsiGuild.giveawayRole == null || it.getRoleOrNull(obsiGuild.giveawayRole ?: Snowflake(0)) == null) {
+                        obsiGuild.giveawayRole = it.createRole {
+                            name = "Giveaway Permissions"
+                            reason = "Created by obsi-bot (please don't delete)"
+                        }.id
+                    }
                 }
             }
         }
