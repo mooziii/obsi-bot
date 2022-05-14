89cc6d4ecf94eb0d1ab07edac515968f7046e9bb
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-14 11:15:03 +0200

    Giveaway command works!

--- src/main/kotlin/me/obsilabor/obsibot/commands/GiveawayCommand.kt
+++ src/main/kotlin/me/obsilabor/obsibot/commands/GiveawayCommand.kt
@@ -10,38 +10,52 @@ import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
 import com.kotlindiscord.kord.extensions.types.respondEphemeral
 import com.kotlindiscord.kord.extensions.types.respondPublic
+import com.kotlindiscord.kord.extensions.utils.hasRole
 import dev.kord.common.annotation.KordPreview
 import dev.kord.core.behavior.channel.createMessage
 import dev.kord.rest.builder.message.create.actionRow
+import dev.kord.rest.builder.message.create.embed
 import me.obsilabor.obsibot.ObsiBot
 import me.obsilabor.obsibot.localization.globalText
+import me.obsilabor.obsibot.localization.localText
+import me.obsilabor.obsibot.utils.applyDefaultFooter
+import me.obsilabor.obsibot.utils.createObsiGuild
+import me.obsilabor.obsibot.utils.hasRole
+import me.obsilabor.obsibot.utils.obsify
 
 @KordPreview
 class GiveawayCommand : Extension() {
     override val name: String = "giveawaycommand"
 
     override suspend fun setup() {
-        publicSlashCommand {
+        ephemeralSlashCommand {
             name = "giveaway"
             description = globalText("command.giveaway.description")
 
             guild(ObsiBot.TEST_SERVER_ID)
 
-            publicSubCommand(::GiveawayCreateArgs) {
+            ephemeralSubCommand(::GiveawayCreateArgs) {
                 name = "create"
                 description = globalText("command.giveaway.description")
 
                 action {
-                    respondEphemeral {
-                        content = "hi"
-                        actionRow {
-                            selectMenu("smiley") {
-                                option("mache ein smiley", "smiley") {
-                                    description = "vllt machst du ein smiley zu viel"
-                                }
-                                option("tjaa", "tja") {
-                                    description = "tjaa"
-                                }
+                    val obsiGuild = guild?.asGuildOrNull()?.obsify() ?: guild?.asGuildOrNull()?.createObsiGuild()
+                    if(obsiGuild == null) {
+                        respond {
+                            content = "obsiGuild == null"
+                        }
+                        return@action
+                    }
+                    if(member?.asMember()?.hasRole(obsiGuild.giveawayRole) == true) {
+                        respond {
+                            content = "hi"
+                        }
+                    } else {
+                        respond {
+                            embed {
+                                title = localText("generic.nopermissions.short", obsiGuild)
+                                description = localText("command.giveaway.rolerequired", obsiGuild)
+                                applyDefaultFooter()
                             }
                         }
                     }
