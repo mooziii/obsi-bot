da107e29676e5029825ee19f938db771592f261a
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-14 10:51:59 +0200

    Create Member.hasRole(Snowflake) extension

--- src/main/kotlin/me/obsilabor/obsibot/utils/DiscordExtensions.kt
+++ src/main/kotlin/me/obsilabor/obsibot/utils/DiscordExtensions.kt
@@ -1,7 +1,10 @@
 package me.obsilabor.obsibot.utils
 
+import com.kotlindiscord.kord.extensions.utils.hasRole
 import dev.kord.common.annotation.KordPreview
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.entity.Guild
+import dev.kord.core.entity.Member
 import me.obsilabor.obsibot.data.ObsiGuild
 import me.obsilabor.obsibot.database.MongoManager
 import org.litote.kmongo.eq
@@ -17,4 +20,9 @@ fun Guild.createObsiGuild(): ObsiGuild {
     val obsiGuild = ObsiGuild.newDocument(id)
     MongoManager.guilds.insertOne(obsiGuild)
     return obsiGuild
+}
+
+@KordPreview
+suspend fun Member.hasRole(role: Snowflake): Boolean {
+    return hasRole(guild.getRole(role))
 }
