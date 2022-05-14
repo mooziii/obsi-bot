4ceae7ce6a681e3eb58ef9831621f16fd39be6f5
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-14 10:51:09 +0200

    Remove unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ version = "1.0.0"
 repositories {
     mavenCentral()
     maven("https://maven.kotlindiscord.com/repository/maven-public/")
-    //maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 kotlin.sourceSets.all {
@@ -29,7 +28,6 @@ dependencies {
     implementation("org.litote.kmongo", "kmongo-core", "4.5.1")
     implementation("org.litote.kmongo", "kmongo-serialization-mapping", "4.5.1")
     // kord and kordex libraries
-    //implementation("dev.kord:kord-core:0.8.0-M13-SNAPSHOT")
     implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.5.2-RC1")
     // utility libraries
     implementation("net.sf.jopt-simple:jopt-simple:5.0.4")
