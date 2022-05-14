75c4ef3245f1180092c6d2ecc749f4d5b289fa06
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-14 11:13:33 +0200

    Encode to json using custom json object [skip ci]

--- src/main/kotlin/me/obsilabor/obsibot/config/ConfigManager.kt
+++ src/main/kotlin/me/obsilabor/obsibot/config/ConfigManager.kt
@@ -2,7 +2,7 @@ package me.obsilabor.obsibot.config
 
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+import me.obsilabor.obsibot.ObsiBot
 import java.io.File
 
 object ConfigManager {
@@ -10,13 +10,10 @@ object ConfigManager {
     val mongoConfig: MongoConfig
     get() {
         val file = File("mongodb.json")
-        if(!file.parentFile.exists()) {
-            file.parentFile.mkdirs()
-        }
         if(!file.exists()) {
             file.createNewFile()
-            file.writeText(Json.encodeToString(MongoConfig("127.0.0.1", 27017, "database", "user", "password")))
+            file.writeText(ObsiBot.json.encodeToString(MongoConfig("127.0.0.1", 27017, "database", "user", "password")))
         }
-        return Json.decodeFromString(file.readText())
+        return ObsiBot.json.decodeFromString(file.readText())
     }
 }
