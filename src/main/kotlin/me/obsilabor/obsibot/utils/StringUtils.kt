2e8eb6e70413c4f478a99840132846a1c09631bb
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-15 14:39:19 +0200

    Create StringUtils.kt

---
+++ src/main/kotlin/me/obsilabor/obsibot/utils/StringUtils.kt
@@ -0,0 +1,12 @@
+package me.obsilabor.obsibot.utils
+
+object StringUtils {
+
+    fun getRandomID(): String {
+        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+        return (1..6)
+            .map { allowedChars.random() }
+            .joinToString("")
+    }
+
+}
