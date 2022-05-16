61326852089705efd57a6f2b6b3e1f5a29cde1c1
mooziii <63669478+mooziii@users.noreply.github.com>
2022-05-16 14:17:29 +0200

    Update Poll.kt

--- src/main/kotlin/me/obsilabor/obsibot/data/Poll.kt
+++ src/main/kotlin/me/obsilabor/obsibot/data/Poll.kt
@@ -4,9 +4,13 @@ import dev.kord.common.entity.Snowflake
 
 @kotlinx.serialization.Serializable
 data class Poll(
+    val guildId: Snowflake,
+    val channelId: Snowflake,
     val interactionId: String,
     val messageId: Snowflake,
     val owner: Snowflake,
     val options: HashMap<String, Int>,
-    val end: Long
+    val end: Long,
+    val voters: HashMap<Snowflake, String>,
+    val ended: Boolean
 )
