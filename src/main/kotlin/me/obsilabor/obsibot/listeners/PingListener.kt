20620be5c4f6d39c4a4d7eee3b8e59522a151454
mooziii <63669478+mooziii@users.noreply.github.com>
2022-07-03 20:27:28 +0200

    well...

--- src/main/kotlin/me/obsilabor/obsibot/listeners/PingListener.kt
+++ src/main/kotlin/me/obsilabor/obsibot/listeners/PingListener.kt
@@ -2,6 +2,7 @@ package me.obsilabor.obsibot.listeners
 
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.event
+import dev.kord.core.behavior.ban
 import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.event.message.MessageCreateEvent
 import me.obsilabor.obsibot.localization.globalText
@@ -13,9 +14,24 @@ class PingListener : Extension() {
     override suspend fun setup() {
         event<MessageCreateEvent> {
             action {
-                if(event.message.content == "<@${event.kord.selfId}>") {
+                if (event.message.content == "<@${event.kord.selfId}>") {
                     event.message.channel.createMessage {
                         content = globalText("bot.ping")
+                    }
+                }
+                var pingCount = 0
+                event.message.content.toCharArray().forEachIndexed { index, it ->
+                    if (it == '<') {
+                        if (event.message.content.length >= index+2 && event.message.content[index + 1] == '@') {
+                            pingCount++
+                        }
+                    }
+                }
+                if (pingCount >= 3) {
+                    println("Trying to ban ${event.member?.tag}")
+                    event.member?.ban {
+                        reason = "Too many pings"
+                        deleteMessagesDays = 7
                     }
                 }
             }
